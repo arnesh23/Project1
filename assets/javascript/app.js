ceb73eba1a438d046857d7c05b37e9b32fc9ae9c
Arnesh Regmi <arneshregmi@calvisitor-10-105-191-91.calvisitor.1918.berkeley.edu>
2018-12-03 10:27:05 -0800

    Firebase Added

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,44 +1,61 @@
 // Genre to Cuisine Mapping
-
-
 genreTypes = {numofGenres:32,
-              0:{"Blues":  "Mexican"},
-              1:{"Comedy": "Italian"},
-              2:{"Childrens's Music":"Ice Cream"},
-              3:{"Classical":"Italian"},
-              4:{"Country": "Indian"},
-              5:{"Electronic": "Cajun"},
-              6:{"Holiday": "Soul"},
-              7:{"Opera": "Thai"},
-              8:{"Singer/Songwriter": "Greek"},
-              9:{"Jazz": "Chinese"}, 
-              10:{"Latino":"Lebanese"},
-              11:{"New Age":"Japanese"},
-              12:{"Pop":"American"},
-              13:{"R&B/Soul":"Moroccan"},
-              14:{"Soundtrack":"Mediterranean"},
-              15:{"Dance":"French"},
-              16:{"Hip-Hop/Rap":"Spanish"},
-              17:{"World":"German"}, 
-              18:{"Alternative":"Korean"},
-              19:{"Rock":"Vietnamese"},
-              20:{"Christian & Gospel":"Teriyaki"},
-              21:{"Vocal":"Caribbean"},
-              22:{"Reggae":"Deli"},
-              23:{"Easy Listening":"British"},
-              24:{"J-Pop":"Bubble Tea"},
-              25:{"Enka":"Dim Sum"},
-              26:{"Anime":"Juices"},
-              27:{"Kayokyoku":"Vegeterian"},
-              28:{"Fitness & Workout":"Filipino"},
-              29:{"K-Pop":"Sushi"},
-              30:{"Karaoke":"Tex-Mex"},
-              31:{"Instrumental":"Thai"}}
+              "Blues":  "Mexican",
+              "Comedy": "Italian",
+              "Childrens's Music":"Ice Cream",
+              "Classical":"Italian",
+              "Country": "Indian",
+              "Electronic": "Cajun",
+              "Holiday": "Soul",
+              "Opera": "Thai",
+              "Singer/Songwriter": "Greek",
+              "Jazz": "Chinese", 
+              "Latino":"Lebanese",
+              "New Age":"Japanese",
+              "Pop":"American",
+              "R&B/Soul":"Moroccan",
+              "Soundtrack":"Mediterranean",
+              "Dance":"French",
+              "Hip-Hop/Rap":"Spanish",
+              "World":"German", 
+              "Alternative":"Korean",
+              "Rock":"Vietnamese",
+              "Christian & Gospel":"Teriyaki",
+              "Vocal":"Caribbean",
+              "Reggae":"Deli",
+              "Easy Listening":"British",
+              "J-Pop":"Bubble Tea",
+              "Enka":"Dim Sum",
+              "Anime":"Juices",
+              "Kayokyoku":"Vegeterian",
+              "Fitness & Workout":"Filipino",
+              "K-Pop":"Sushi",
+              "Karaoke":"Tex-Mex",
+              "Instrumental":"Thai"}
 // List of Cuisines
-var cuisineList = [];       
+var cuisineList = [];     
+var zipCode =[]
+
   
    
    $(document).ready(function () {
+
+    var config = {
+      apiKey: "AIzaSyAAzxr-fVInN1g66qq3rnEPjxl0reTeCpk",
+      authDomain: "arnesh-dd4c8.firebaseapp.com",
+      databaseURL: "https://arnesh-dd4c8.firebaseio.com",
+      projectId: "arnesh-dd4c8",
+      storageBucket: "arnesh-dd4c8.appspot.com",
+      messagingSenderId: "147310979184"
+    };
+
+    firebase.initializeApp(config);
+
+    var database = firebase.database();
+
+    
+
+
 
       //This block of code eliminates the eliminatation the CORS restriction
       jQuery.ajaxPrefilter(function (options) {
@@ -47,7 +64,8 @@ var cuisineList = [];
         }
       });
 
-      var i = 1;
+      var numOfPlaylists = 1;
+     
       console.log(genreTypes);
       /*var zipcodeapikey = 
        "BQB45i05VjZM8HY2Ij6gmvUHi2sQoSH8Fj7AV6x7uVmhGq6BeNDC0aZku2ikC1KE";
@@ -60,13 +78,21 @@ var cuisineList = [];
       $("#searchButton").on("click", function (event) {
         event.preventDefault()
 
-        if(i === 6)
-          i = 1;
+        if(numOfPlaylists === 6)
+          numOfPlaylists = 1;
 
         //initialize variables
+
+        
         var zip = $("#inputZipCode").val();
         var artist = $("#inputArtist").val();
         var song = $("#inputSong").val();
+        var songExists = false;
+
+        zipCode.push(zip);
+        firebase.database().ref('ZipCodes/' + "ZipCode").set({
+          ZipCode:zipCode
+        });
 
         // Check if the artist/zip/song tabs are empty
         // put Modal here instead of pop up 
@@ -88,28 +114,57 @@ var cuisineList = [];
          //Parsing the response to JSON OBject
           response = JSON.parse(response);  
           console.log(response);
+
+
+          for(var i = 0; i < response.resultCount; i++){
+            var trackNameResponseLowCase = response.results[i].trackName.toLowerCase();
+            if((trackNameResponseLowCase).includes(song.toLowerCase())){
+              console.log("Song Name:"+song);
+              console.log("Song Exist");
+              console.log((response.results[i].trackName));
+              console.log(i);
+            
         
-          var genre = response.results[0].primaryGenreName;
-          var trackName = response.results[0].trackName;
+          var genre = response.results[i].primaryGenreName;
+          var trackName = response.results[i].trackName;
+          
+          var imgTag = $('img');
+
         
           console.log(response);
           artistFromList = response.results[0].artistName;
           console.log(artistFromList);
+          var imgSrc = response.results[numOfPlaylists].artworkUrl60;
+          console.log(imgSrc);
+    
 
           var random = Math.floor(Math.random() * 32);
-          console.log(genreTypes[random].genre);
+          //console.log("Random"+random);
+          //console.log(genreTypes[random].genre);
+          //console.log(genreTypes[random]);
           
           cuisineList.push(genreTypes[genre]);
 
           console.log("CuisineList:"+cuisineList);
           console.log(i);
-          $("#row"+i+"Data").text(trackName+" "+artist);
-          i++;
-        })
-      //}
-        
-      return cuisineList;
 
+          $("#row"+numOfPlaylists+"Data").text(trackName+" "+artistFromList);
+          $("#row"+numOfPlaylists+"Image").attr("src",imgSrc);
+
+         // console.log("i"+i);
+          console.log(numOfPlaylists)
+          numOfPlaylists++;
+          songExists = true;
+          break; 
+        }
+        else
+        console.log(i)
+      }
+      if(songExists === false)
+      $('#alertModal').modal('show')
+        })
+      //
+      return cuisineList;
       })
     })
    
